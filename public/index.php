1144ec98ce98c8586d0e5f013bc0ebeddba81175
mehedi-hasan-shuvon <mehedi.hasan.shuvon@gmail.com>
2023-05-24 10:27:50 +0600

    removing unnecessary imports

--- public/index.php
+++ public/index.php
@@ -6,10 +6,6 @@ use Project\Routes\HomeRoutes;
 
 
 
-header("Access-Control-Allow-Origin: http://localhost:8888");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
 
 new HomeRoutes();
 
